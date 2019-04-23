e76457623607016d4a59f101958696c3dd97ec8c
Mohan <mohankandasami@gmail.com>
2019-04-23 13:54:38 -0400

    update test.tf

--- test.tf
+++ test.tf
@@ -1,4 +1,6 @@
-provider "aws" provider "aws" {}
+provider "aws" provider "aws" {
+  region                  = "us-east-2"  
+}
 
 resource "aws_s3_bucket" "b" {
   bucket = "mk-e2-s3-bkt-terraform"
