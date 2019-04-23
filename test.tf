b1f28647fe6d73cbfebbbd482648c2bd1eafdb87
Mohan <mohankandasami@gmail.com>
2019-04-23 13:48:49 -0400

    create test.tf

---
+++ test.tf
@@ -0,0 +1,10 @@
+provider "aws" provider "aws" {}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "mk-e2-s3-bkt-terraform"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
